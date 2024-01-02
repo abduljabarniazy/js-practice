cf7c71bf4fc45f0fa05b0cbeaff8568feb58b1f2
Abduljabar <57216932+abduljabarniazy@users.noreply.github.com>
2024-01-02 07:03:19 +0000

    code writed in this file

--- 02_const_let_var.js
+++ 02_const_let_var.js
@@ -1,1 +1,28 @@
 
+const name="Abdul Jabar"    //in this type of variable we can not override another value 
+
+let sureName="Niazy"        // in this type of variable we can override another value
+
+var age = 25                 // in this type of variable we can override another value, not prefer to use
+
+city = "Jalalabad"               // this is the oldest method of variable, we never use this type of variable
+
+
+
+console.log(name)
+
+console.log(sureName)
+
+console.log(age)
+
+console.log(city)
+
+// overriding
+// name="Abdul"   //we can not override the const type of varialble
+
+ sureName= "Hello" 
+ age= 32
+ city= "kabul"
+
+ console.table([name, sureName, age, city])
+
