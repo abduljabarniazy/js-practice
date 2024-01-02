530b619ae9d62b199a2138ced990df76b0e3b55b
Abduljabar <57216932+abduljabarniazy@users.noreply.github.com>
2024-01-02 07:27:24 +0000

    code for datatypes

--- 03_DataTypes.js
+++ 03_DataTypes.js
@@ -1,1 +1,14 @@
 
+
+
+let Name="Abdul Jabar"          // string type of Variable
+let Age = 45;                   // number type of variable
+let logedIn= true               //
+let live=undefined
+let score=null
+
+console.log(typeof Name)
+console.log(typeof Age)
+console.log(typeof logedIn)
+console.log(typeof live)
+console.log(typeof score)
